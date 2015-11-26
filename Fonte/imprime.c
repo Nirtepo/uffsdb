0d641663a43cd4ed99aae935b77aea5fddde92e1
Guilherme Antunes Da Silva <nirtepo@yandex.ru>
2015-11-26 18:32:55 -0200

    imprime.c  ->1
    Added imprimese function.

--- Fonte/imprime.c
+++ Fonte/imprime.c
@@ -6,6 +6,109 @@
     Parametros: Nome da tabela (char).
     Retorno:    void.
    ---------------------------------------------------------------------------------------------*/
+void imprimese(char nomeTabela[], char type[]){
+	int j,erro, x, p, cont=0, n;
+    struct fs_objects objeto;
+    struct clauses *constr = NULL;
+    clauses_get(type, &constr);
+    if(clauses_check(constr, nomeTabela)){
+	    /*
+	    str_chain_add(&str,"um");
+        str_chain_add(&str,"dois"); 
+        str_chain_clear(str);*/
+
+        if(!verificaNomeTabela(nomeTabela)){
+            printf("\nERROR: relation \"%s\" was not found.\n\n\n", nomeTabela);
+            return;
+        }
+
+    	objeto = leObjeto(nomeTabela);
+
+    	tp_table *esquema = leSchema(objeto);
+
+    	if(esquema == ERRO_ABRIR_ESQUEMA){
+    	    printf("ERROR: schema cannot be created.\n");
+    	    free(esquema);
+    	    return;
+    	}
+
+    	tp_buffer *bufferpoll = initbuffer();
+
+    	if(bufferpoll == ERRO_DE_ALOCACAO){
+    	    free(bufferpoll);
+    	    free(esquema);
+    	    printf("ERROR: no memory available to allocate buffer.\n");
+    	    return;
+    	}
+
+    	erro = SUCCESS;
+    	for(x = 0; erro == SUCCESS; x++)
+    	    erro = colocaTuplaBuffer(bufferpoll, x, esquema, objeto);
+
+    	int ntuples = --x;
+		p = 0;
+		n=-1;
+		int chk;
+		while(x){
+		    column *pagina = getPage(bufferpoll, esquema, objeto, p);
+		    if(pagina == ERRO_PARAMETRO){
+    	        printf("ERROR: could not open the table.\n");
+    	        free(bufferpoll);
+    	        free(esquema);
+    	        return;
+		    }
+
+		    if(!cont) {
+		        for(j=0; j < objeto.qtdCampos; j++){
+		            if(pagina[j].tipoCampo == 'S')
+		                printf(" %-20s ", pagina[j].nomeCampo);
+		        	else
+		                printf(" %-10s ", pagina[j].nomeCampo);
+		            if(j<objeto.qtdCampos-1)
+		            	printf("|");
+		        }
+		        printf("\n");
+		        for(j=0; j < objeto.qtdCampos; j++){
+		            printf("%s",(pagina[j].tipoCampo == 'S')? "----------------------": "------------");
+		            if(j<objeto.qtdCampos-1)
+		            	printf("+");
+		        }
+		        printf("\n");
+		    }
+		    cont++;       
+    
+			for(j=0; j < objeto.qtdCampos*bufferpoll[p].nrec; j++){
+	        	if(n<(int)j/objeto.qtdCampos){
+					n++;
+					chk = checkPageLine(pagina, &objeto, bufferpoll, constr,n);
+				}
+				if(chk){ 
+					if(pagina[j].tipoCampo == 'S')
+		 	           	printf(" %-20s ", pagina[j].valorCampo);
+		        	else if(pagina[j].tipoCampo == 'I'){
+		            	int *n = (int *)&pagina[j].valorCampo[0];
+		            	printf(" %-10d ", *n);
+		        	}
+					else if(pagina[j].tipoCampo == 'C'){
+		            	printf(" %-10c ", pagina[j].valorCampo[0]);
+		        	} 
+					else if(pagina[j].tipoCampo == 'D'){
+		            	double *n = (double *)&pagina[j].valorCampo[0];
+		    	        printf(" %-10f ", *n);
+		        	}
+	            	if(j>=1 && ((j+1)%objeto.qtdCampos)==0)
+            			printf("\n");
+        			else
+        				printf("|");
+				}
+    		}
+			x-=bufferpoll[p++].nrec;
+		}
+    	printf("\n(%d %s)\n\n",ntuples,(1>=ntuples)?"row": "rows");
+		free(bufferpoll);
+	    free(esquema);
+	}
+}
 
 void imprime(char nomeTabela[]) {
 
