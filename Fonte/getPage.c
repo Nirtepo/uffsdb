0acac55ae0dc5bc75fe191df8d1b6d31d3144a47
Guilherme Antunes Da Silva <nirtepo@yandex.ru>
2015-11-26 18:36:20 -0200

    getPage.c  ->1
    Added getOnPageLine and checkPageLine functions

--- Fonte/getPage.c
+++ Fonte/getPage.c
@@ -46,3 +46,43 @@ column * getPage(tp_buffer *buffer, tp_table *campos, struct fs_objects objeto, 
 
     return colunas; //Retorna a 'page' do buffer
 }
+
+column * getOnPageLine(column * page, struct fs_objects *objeto, tp_buffer *bufferpool, char * attr, int n){
+	int j;
+	for(j=0+(n*objeto->qtdCampos); j < ((n+1)*objeto->qtdCampos); j++)
+		if(strcmp(page[j].nomeCampo, attr)==0)
+			return &page[j];		
+	return NULL;
+}
+
+int checkPageLine(column * page, struct fs_objects *objeto, tp_buffer *bufferpool, struct clauses * claus, int n){
+	column *ret1, *ret2;
+	while(claus!=NULL){
+		ret1 = getOnPageLine(page, objeto, bufferpool, claus->attr1, n);
+		ret2 = getOnPageLine(page, objeto, bufferpool, claus->attr2, n);
+		if(ret1==NULL||ret2==NULL)
+			return 0; 
+		if((ret1->tipoCampo=='D'&&ret2->tipoCampo=='D')||(ret1->tipoCampo=='I'&&ret2->tipoCampo=='D')||(ret1->tipoCampo=='D'&&ret2->tipoCampo=='I')){
+			if(!generalCmp(ret1->valorCampo, ret2->valorCampo, 'D', claus->comp))
+				return 0;		
+		}
+		else if(ret1->tipoCampo=='I'&&ret2->tipoCampo=='I'){
+			if(!generalCmp(ret1->valorCampo, ret2->valorCampo, 'I', claus->comp))
+				return 0; 
+		}
+		else if(ret1->tipoCampo=='C'&&ret2->tipoCampo=='C'){
+			if(!generalCmp(ret1->valorCampo, ret2->valorCampo, 'C', claus->comp))
+				return 0;
+		}
+		else if(ret1->tipoCampo=='S'&&ret2->tipoCampo=='S'){
+			if(!generalCmp(ret1->valorCampo, ret2->valorCampo, 'S', claus->comp))
+				return 0;
+		}
+		else{
+			printf("The attributes '%s'%s and '%s'%s are incompatible", ret1->nomeCampo,(ret2->tipoCampo=='C'? "char" : (ret2->tipoCampo=='I'? "integer":(ret2->tipoCampo=='D'? "double":(ret2->tipoCampo=='S'? "string" : "unknown")))), ret2->nomeCampo, (ret2->tipoCampo=='C'? "char" : (ret2->tipoCampo=='I'? "integer":(ret2->tipoCampo=='D'? "double":(ret2->tipoCampo=='S'? "string" : "unknown")))));			
+			return 0;		
+		}
+		claus = claus->next;
+	}
+	return 1;
+}
